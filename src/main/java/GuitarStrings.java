1f297281fe947591a2d8c0459186020b7dd4aded
User <user@Users-MacBook-Pro.local>
2020-02-07 16:13:24 +0000

    guitarstrings and sheet music class added

---
+++ src/main/java/GuitarStrings.java
@@ -0,0 +1,8 @@
+public class GuitarStrings extends Accessories{
+
+
+    public GuitarStrings(String name, double buyPrice, double sellPrice, String description) {
+        super(name, buyPrice, sellPrice, description);
+
+    }
+}
---
+++ src/main/java/SheetMusic.java
@@ -0,0 +1,7 @@
+public class SheetMusic extends Accessories {
+
+
+    public SheetMusic(String name, double buyPrice, double sellPrice, String description) {
+        super(name, buyPrice, sellPrice, description);
+    }
+}
