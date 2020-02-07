9f40a21932c882f3e8f6c5e39cda872cde58a907
User <user@Users-MacBook-Pro.local>
2020-02-07 21:16:45 +0000

    ShopTest class added, stockCount added

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,12 +1,14 @@
+import instruments.IPlay;
+
 import java.util.ArrayList;
 
-public class Shop {
+public class Shop implements ISell, IPlay {
 
     private ArrayList<ISell> stock;
 
 
-    public Shop(ArrayList<ISell> stock) {
-        this.stock = stock;
+    public Shop() {
+        this.stock = new ArrayList<>();
     }
 
 
@@ -17,5 +19,19 @@ public class Shop {
 
     public void removeItem(ISell item) {
         this.stock.add(item);
+    }
+
+    @Override
+    public double calculateMarkup() {
+        return 0;
+    }
+
+    @Override
+    public String play() {
+        return null;
+    }
+
+    public int countStock() {
+        return this.stock.size();
     }
 }
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,53 @@
+import com.sun.tools.javac.jvm.Items;
+import instruments.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopTest {
+
+    private Shop shop;
+    private Guitar guitar1;
+    private Guitar guitar2;
+    private Piano piano1;
+    private Piano piano2;
+    private Drum drum1;
+    private Drum drum2;
+    private SheetMusic sheetMusic1;
+    private SheetMusic sheetMusic2;
+    private GuitarStrings guitarStrings1;
+    private GuitarStrings guitarStrings2;
+
+
+    @Before
+    public void before(){
+        guitar1 = new Guitar("Wood", "Sunburst", "Twang", InstrumentType.STRINGS, 300, 400, 6);
+        guitar2 = new Guitar("Pine", "Fender Red", "Strum", InstrumentType.STRINGS, 200, 500, 6);
+        piano1 = new Piano("Ivory", "Black", "Plinky Plonky", InstrumentType.WOODWIND, 1000, 1500, 88);
+        piano2 = new Piano("Oak", "White", "Plonky Plinky", InstrumentType.WOODWIND, 2000, 2500, 89);
+        drum1 = new Drum("Steel", "Blue", "Tippy Tap", InstrumentType.PERCUSSION, 200, 300, 5);
+        drum2 = new Drum("Brass", "Green", "Clank", InstrumentType.PERCUSSION, 500, 700, 6);
+        sheetMusic1 = new SheetMusic(10, 26, "Best of Dylan");
+        sheetMusic2 = new SheetMusic(15, 30, "Best of Springsteen");
+        guitarStrings1 = new GuitarStrings(8, 16, "Brass medium strings");
+        guitarStrings2 = new GuitarStrings(11, 20, "Bronze easy strings");
+        shop = new Shop();
+
+
+    }
+
+//    @Test
+//    public void buyItems(){
+//        shop.addItem();
+//        assertEquals(1, shop.countStock());
+//
+//    }
+
+    @Test
+    public void stockCount(){
+        assertEquals(0, shop.countStock());
+    }
+}
